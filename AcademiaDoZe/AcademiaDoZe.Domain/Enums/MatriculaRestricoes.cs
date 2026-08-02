1fa698402024237bfd1f362c82477b08b800e59f
Hibrael Xavier <hibraelxavier@gmail.com>
2026-08-02 18:44:06 -0300

    Correção enum MatriculaRestricoes

--- AcademiaDoZe/AcademiaDoZe.Domain/Enums/MatriculaRestricoes.cs
+++ AcademiaDoZe/AcademiaDoZe.Domain/Enums/MatriculaRestricoes.cs
@@ -4,21 +4,17 @@ using System.Text;
 
 namespace AcademiaDoZe.Domain.Enums
 {
-    internal class MatriculaRestricoes
-    {
         [Flags]
         public enum MatriculaRestricoes
         {
             Nenhuma = 0,
             Diabetes = 1,
             Labirintite = 2,
-            ProblemasRespiratórios = 4,
+            ProblemasRespiratorios = 4,
             RemedioContinuo = 8,
             ProblemasCardiacos = 16,
             ProblemasOsseos = 32,
             CirurgiaDebilitante = 64,
 
         }
-        readonly
-    }
 }
