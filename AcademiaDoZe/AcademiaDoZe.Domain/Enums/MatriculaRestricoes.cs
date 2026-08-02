5d4b399da403d07d65918774f4fb0ff389dec68d
Hibrael Xavier <hibraelxavier@gmail.com>
2026-08-02 18:29:26 -0300

    Classe MatriculaRestricoes

--- AcademiaDoZe/AcademiaDoZe.Domain/Enums/MatriculaRestricoes.cs
+++ AcademiaDoZe/AcademiaDoZe.Domain/Enums/MatriculaRestricoes.cs
@@ -6,5 +6,19 @@ namespace AcademiaDoZe.Domain.Enums
 {
     internal class MatriculaRestricoes
     {
+        [Flags]
+        public enum MatriculaRestricoes
+        {
+            Nenhuma = 0,
+            Diabetes = 1,
+            Labirintite = 2,
+            ProblemasRespiratórios = 4,
+            RemedioContinuo = 8,
+            ProblemasCardiacos = 16,
+            ProblemasOsseos = 32,
+            CirurgiaDebilitante = 64,
+
+        }
+        readonly
     }
 }
