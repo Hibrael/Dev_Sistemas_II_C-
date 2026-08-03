28760166551099c7d70dabf64ff4812c82bbf658
Hibrael Xavier <hibraelxavier@gmail.com>
2026-08-03 19:29:34 -0300

    Padronização AcessoColborador

--- AcademiaDoZe/AcademiaDoZe.Domain/Entities/AcessoColaborador.cs
+++ AcademiaDoZe/AcademiaDoZe.Domain/Entities/AcessoColaborador.cs
@@ -5,42 +5,18 @@ namespace AcademiaDoZe.Domain.Entities
 {
     public class AcessoColaborador : Entity
     {
-        public int ColaboradorId { get; private set; }
-        public string Codigo { get; private set; }
-        public bool Ativo { get; private set; }
+        public Colaborador Colaborador { get; private set; }
+        public DateTime DataHora { get; private set; }
 
-        private AcessoColaborador(int colaboradorId, string codigo)
+        private AcessoColaborador(Colaborador colaborador)
         {
-            ColaboradorId = colaboradorId;
-            Codigo = codigo;
-            Ativo = true;
+            Colaborador = colaborador;
+            DataHora = DateTime.UtcNow  ;
         }
 
-        public static AcessoColaborador Criar(int colaboradorId, string codigo)
+        public static AcessoColaborador Criar(Colaborador colaborador)
         {
-            if (colaboradorId <= 0)
-                throw new ArgumentException("Colaborador inválido.");
-
-            if (string.IsNullOrWhiteSpace(codigo))
-                throw new ArgumentException("Código de acesso é obrigatório.");
-
-            return new AcessoColaborador(colaboradorId, codigo);
-        }
-
-        public void Bloquear()
-        {
-            if (!Ativo)
-                throw new InvalidOperationException("Acesso já está bloqueado.");
-
-            Ativo = false;
-        }
-
-        public void Liberar()
-        {
-            if (Ativo)
-                throw new InvalidOperationException("Acesso já está liberado.");
-
-            Ativo = true;
+            return new AcessoColaborador(colaborador);
         }
     }
 }
